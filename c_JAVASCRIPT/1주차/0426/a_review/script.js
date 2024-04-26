d89678b35a9e61535ebdbad93e24a3c1b1df1a82
devgiants75 <devgiants75@naver.com>
2024-04-26 20:50:51 +0900

    20240426

--- c_JAVASCRIPT/1주차/0426/a_review/script.js
+++ c_JAVASCRIPT/1주차/0426/a_review/script.js
@@ -15,25 +15,39 @@
 
 //& --------코드 작성--------//
 // 사용자로부터 섭씨 온도 입력받기
-let celsius;
+let celsius = Number(prompt('섭씨 온도를 입력해주세요.'));
 
-// 섭씨를 화씨로 변환
-let fahrenheit;
+// 섭씨를 화씨로 변환(화씨 = 섭씨 X 9 / 5 + 32)
+let fahrenheit = celsius * 9 / 5 + 32;
 
 // 온도에 따른 메시지 결정
 let message;
 
+if (celsius <= 0) {
+  message = '매우 추워요!';
+} else if (celsius < 25) {
+  message = '시원하네요!';
+} else { // 25이상의 섭씨 기온
+  message = '꽤 덥습니다!';
+}
+
 // 결과 출력
-console.log();
+console.log(`화씨 온도 ${fahrenheit} - ${message}`);
 
 //! 문제 2. 입력 값 검증 (if문)
 // 1. age 변수에 prompt 창('Please enter your age.')에서 받아오기
 // 2. 해당 age 변수 값이 0미만이거나 120초과일 경우
 // 3. 콘솔창에 Invalid age.를 출력
 
-let age;
+let age = Number(prompt('나이를 입력해주세요.'));
 
-//? 3. 요일에 따른 활동 추천(switch문)
+if (age < 0 || age > 120) { // 점수 계산 로직과 유사
+  console.log('Invalid age');
+  console.error('Invalid age');
+}
+console.log('정상적인 코드 흐름');
+
+//! 문제 3. 요일에 따른 활동 추천(switch문)
 // 1. 현재 요일에 따라 다른 활동을 추천
 
 // Date()함수의 getDay() 메서드를 사용
@@ -48,4 +62,40 @@ console.log(day); // 오늘 날짜를 기준으로 5(금요일) 출력
 // 6: 토요일 "토요일! 주말의 시작!"
 // default: "Invalid day."
 
-//! 삼항연산자 예제(배경색 변경)
+switch (day) { // day: number 타입
+  case 0:
+    console.log('일요일');
+    break;
+  case 1: // case 1 ~ 5까지는 break가 없는 동일한 코드를 출력
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+    console.log('평일');
+    break;
+  case 6:
+    console.log('토요일');
+    break;
+  default:
+    console.log('유효하지 않은 요일입니다.');
+}
+
+//! 문제4: 삼항연산자 예제(배경색 변경)
+
+const body = document.querySelector('body');
+const select = document.querySelector('select');
+const div = document.querySelector('div');
+
+document.body.style.padding = '10px';
+div.style.margintTop = '100px';
+
+function update (bgColor, textColor) {
+  body.style.backgroundColor = bgColor;
+  body.style.color = textColor;
+}
+
+select.onchange = function () {
+  select.value === 'black' 
+    ? update('black', 'white') 
+    : update('white', 'black');
+}
--- c_JAVASCRIPT/1주차/0426/b_review02/script.js
+++ c_JAVASCRIPT/1주차/0426/b_review02/script.js
@@ -1,14 +1,60 @@
 //! 조건문을 사용하는 프로그래밍 로직
 
-//? 1. 사용자 입력값 검사
+//? 1. 사용자 입력값 검사 (유효성 검사)
 // 사용자로부터 입력받은 값이 유효한지 검사하기 위해 조건문 사용
 
 // isNaN() 함수
 // : 어떤 값이 NaN인지 판별
 // : 숫자가 아닌 경우: true, 숫자인 경우: false
 
+let userInput = prompt('숫자를 입력해주세요.');
+
+if (isNaN(userInput)) {
+  console.log('유효한 숫자가 아닙니다.');
+} else {
+  console.log(`당신이 입력한 값은 ${userInput}입니다.`);
+}
+
+userInput = '  아이디';
+
+// trim() 함수
+// : 문자열 양 끝의 공백을 제거한 값을 반환
+
+if (userInput.trim() === '') {
+  console.log('아이디를 입력해주세요.');
+} else {
+  console.log('로그인을 성공하였습니다.');
+}
+
 //? 2. 접근 제어
 // 로그인 상태나 사용자 권한에 따라 다른 페이지를 보여줄 때 조건문 사용
 
+let isLoggedIn = true; // 사용자의 로그인 상태 저장
+let isAdmin = false; // 관리자 여부를 확인하는 상태 저장
+
+if (isLoggedIn) {
+  if (isAdmin) {
+    console.log('관리자님 환영합니다.');
+  } else {
+    console.log('회원님 환영합니다.');
+  }
+
+} else {
+  // 로그인 하지 않을 경우 화면 출력 X
+  console.log('로그인 해주세요.'); 
+}
+
 //? 코드 실행 흐름 제어
 // 조건에 따라서 특정 코드를 실행할지에 대한 여부를 제어 가능
+
+// confirm 함수 (확인하다: 출력문)
+
+let shouldExecute = confirm('해당 코드를 실행하시겠습니까?');
+// : 출력문과 함께 확인 또는 취소의 버튼이 출력
+// : 확인(true), 취소(false) 반환
+
+if (shouldExecute) {
+  console.log('해당 코드가 실행됩니다.');
+} else {
+  console.log('해당 코드는 실행되지 않습니다.');
+}
