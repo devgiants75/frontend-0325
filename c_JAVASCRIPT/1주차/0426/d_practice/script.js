7a574e3b12c6d93686126b605a1c14ef22fdff02
devgiants75 <devgiants75@naver.com>
2024-04-26 16:35:59 +0900

    20240426

---
+++ c_JAVASCRIPT/1주차/0426/d_practice/script.js
@@ -0,0 +1,23 @@
+//! 반복문 연습 예제
+
+//? 1. 배열의 모든 요소를 출력
+
+// for문
+console.log('---for문---');
+
+// while문
+console.log('---while문---');
+
+// do-while문
+console.log('---do-while문---');
+
+//! 반복문 사용하기 실습
+// 사용자로부터 입력 받기(do while문)
+
+// userInput 변수에 propmt('Enter a number greater than 10')
+// userInput 변수가 10을 초과하는 수가 아닌 경우 반복
+
+//! prompt창
+// prompt 함수는 항상 문자열을 반환
+
+//! 반복문 실습
