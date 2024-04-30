e4591bc34d771e8936eaa12a7a066686463f0241
devgiants75 <devgiants75@naver.com>
2024-04-30 21:48:15 +0900

    20240430

--- c_JAVASCRIPT/2주차/0430/c_array/array02.js
+++ c_JAVASCRIPT/2주차/0430/c_array/array02.js
@@ -9,10 +9,32 @@
 
 // - 배열 복사
 // : 배열의 모든 요소를 새 배열에 복사
+// : [...요소를복사할배열명]
+
+let originalArray = [1, 2, 3];
+let copiedArray = [...originalArray];
+console.log(copiedArray); // [ 1, 2, 3 ]
+
+originalArray[1] = 5; // 원본 배열 수정
+console.log(copiedArray); // [ 1, 2, 3 ] - copiedArray는 영향받지 X
+
+let array1 = [11, 22, 33];
+let array2 = array1;
+
+array1[1] = 55;
+console.log(array2); // [ 11, 55, 33 ] - 스프레드 연산자와 달리 원본 배열 수정
 
 // - 배열 병합
 // : 두 개 이상의 배열을 병합할 경우 사용
 // : 병합된 새 배열은 기존 배열들을 변경하지 X
+let number1 = [1, 2, 3];
+let number2 = [4, 5, 6];
+
+let mergedArray = [...number1, ...number2];
+let mergedReverse = [...number2, ...number1];
+
+console.log(mergedArray); // [ 1, 2, 3, 4, 5, 6 ]
+console.log(mergedReverse); // [ 4, 5, 6, 1, 2, 3 ]
 
 //? 깊은 복사와 얕은 복사의 차이
 // 1. 얕은 복사
@@ -26,22 +48,47 @@
 // : 복사된 객체를 변경해도 원본 객체에 영향을 미치지 X
 
 //! 고급 배열 패턴 기능
-// 1. 배열과 문자열 사이의 변환
+//? 배열과 문자열 사이의 변환
 // join()
 // : 배열의 모든 요소를 연결하여 하나의 문자열로 변환
 // : 선택적으로 구분자를 전달 - 제공하지 않을 경우 쉼표가 기본값
+let fruits = ['사과', '바나나', '망고'];
+
+let result = fruits.join(); // 구분자 사용 X: 문자열이 쉼표로 구분
+let resultWithSpace = fruits.join(' '); // 구분자 사용 X: 문자열이 쉼표로 구분
+let resultWithDash = fruits.join('-'); // 구분자 사용 X: 문자열이 쉼표로 구분
+
+console.log(result); // 사과,바나나,망고
+console.log(resultWithSpace); // 사과 바나나 망고
+console.log(resultWithDash); // 사과-바나나-망고
 
 // split()
 // : 문자열을 특정 구분자를 기준으로 분리하여 배열로 반환
+let sentence = '사과,바나나,망고';
+let sentence2 = '사과-바나나-망고';
+
+let words = sentence.split(',');
+let words2 = sentence2.split('-');
+
+console.log(words); // [ '사과', '바나나', '망고' ]
+console.log(words2); // [ '사과', '바나나', '망고' ]
 
 //! 다차원 배열
 // : 배열의 요소로 또 다른 배열을 포함하는 구조
 // : 자바스크립트에서는 주로 2차원 배열을 사용
-//   (3차원 이상의 배열도 존재)'
+//   (3차원 이상의 배열도 존재)
 
 //? 다차원 배열 생성
 // : 배열 리터럴 형식 사용
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+// : 3X3 크기의 2차원 배열
 
 //? 다차원 배열에 접근
-// : 인덱스를 연속적으로 사용
+// : 인덱스를 연속적으로 사용 - [][]
 // : matrix 배열의 첫 번째 행의 두 번째 요소에 접근
+let item = matrix[0][1];
+console.log(item); // 2
---
+++ c_JAVASCRIPT/2주차/0430/c_array/array04.js
@@ -0,0 +1,47 @@
+// 함수를 구현하여, 해당 조건의 로직을 작성하고 결과값을 반환
+// : 함수의 매개변수는 배열의 값을 사용
+
+//! 문제 1: 배열 합계 구하기
+function sumArray(array) {
+  let sum = 0;
+
+  // 배열의 모든 요소에 접근하기 위해
+  // : for 반복문 + 인덱스 번호 사용
+  let length = array.length;
+  for(let i = 0; i < length; i++) {
+    sum += array[i];
+  }
+
+  return sum;
+}
+
+console.log(sumArray([1, 2, 3, 4, 5])); // 15
+console.log(sumArray([11, 43, 23, 75, 12, 32, 13, 64])); // 273
+
+//! 문제 2: 특정 수 이상의 요소 필터링
+function filterGreaterThanTen(array) {
+  let filtered = [];
+  let length = array.length;
+
+  for (let i = 0; i < length; i++) {
+    if (array[i] > 10) {
+      filtered.push(array[i]);
+    }
+  }
+
+  return filtered;
+}
+
+console.log(filterGreaterThanTen([11, 43, 23, 75, 12, 32, 13, 64]));
+// [
+//   11, 43, 23, 75,
+//   12, 32, 13, 64
+// ]
+
+console.log(filterGreaterThanTen([5, 12, 18, 2, 9, 21])); // [ 12, 18, 21 ]
+
+//! 문제 3: 배열 요소 검색
+// : containsElement 함수 구현
+// : 매개변수로 array(배열)와 element(검색값)을 받음
+// - 배열의 모든 요소에 접근하는 동안에 element값과 동일한 값이 있는 경우 true를 반환
+// - element값과 동일한 값이 없는 경우 false를 반환
