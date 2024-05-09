a1cf928bbfda15067e2951c07b3fe4d55d181623
devgiants75 <devgiants75@naver.com>
2024-05-09 20:50:23 +0900

    20240509

--- c_JAVASCRIPT/3주차/0509/b_event/event02.js
+++ c_JAVASCRIPT/3주차/0509/b_event/event02.js
@@ -26,20 +26,47 @@ function random(number) {
 // 이벤트 객체인 e를 함수에 포함하고
 // , 이벤트 객체의 target프로퍼티를 사용하여
 // , 이벤트가 일어나는 버튼 그 자체에 실행
+function bgChange(e) {
+  const randomColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+
+  // JS에서 스타일 지정 방식
+  // : lowerCamelCase를 사용
+  e.target.style.backgroundColor = randomColor;
+}
+
+// 문서에서 id를 사용해 요소 선택
+// : querySelector와의 차이점 - # 사용하지 X
+const colorButton = document.getElementById('colorButton');
+
+const divs = document.querySelectorAll('.colorDiv');
+
+colorButton.addEventListener('click', bgChange);
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bgChange);
+}
 
 // 이벤트 객체 실습
 // : 16개의 타일 세트
-// : querySelectorAll()을 사용하여 16개의 타일의 참조를 div 상수에 할당
+// : querySelectorAll()을 사용하여 16개의 타일의 참조를 tiles 상수에 할당
 // - 반복문을 사용하여 16개의 타일에 이벤트 핸들러를 등록
 // - 각각의 타일을 클릭했을 때 클릭된 타일의 배경색이 변경되도록 지정
 
-function random(number) {
-  return Math.floor(Math.random() * (number + 1));
-}
-
-function bgChange() {
+function tileChange() {
   const randomColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
   return randomColor;
+}
+
+// - 선택자를 사용하여 전체 요소들을 배열로 가져오기
+const tiles = document.querySelectorAll('.tile');
+
+// - 반복문 사용: 각 tile 요소에 onclick 이벤트 핸들러 할당
+// : 해당 이벤트는 이벤트 객체를 전달받기
+// : 해당 객체에서 일어난 요소값을 찾아 색상 부여
+for (let i = 0; i < tiles.length; i++) {
+  tiles[i].onclick = function(e) {
+    e.target.style.backgroundColor = tileChange();
+  }
 }
 
 //! 2. 기본 행동 방지
@@ -47,8 +74,27 @@ function bgChange() {
 // : 비었다면 이벤트 객체에 있는 양식 제출을 멈추도록 지정
 // : 양식 아래에 있는 단락에 에러 메시지를 표시
 
+const form = document.querySelector('form');
+const fname = document.getElementById('fname');
+const email = document.getElementById('email');
+const para = document.querySelector('p');
+
 // querySelector(All): HTML의 요소를 참조
 // getElementById('id값'): 해당 id값을 가진 요소를 참조
+
+form.onsubmit = function(e) {
+  // 이름과 이메일을 작성하는 form 요소
+  // : 이름과 이메일을 반드시 작성하도록 설정
+
+  // 참조된 요소의 값(내용)을 가져올 땐
+  // : 참조값.value 속성을 사용
+  if (fname.value === '' || email.value === '') {
+    // >> input 창의 값은 string 타입이기 때문에
+    //    , 비워질 경우 빈 문자열로 반환
+    e.preventDefault();
+    para.textContent = '이름과 이메일 모두 입력해야만 합니다.';
+  }
+}
 
 //! 3. 이벤트 버블링과 캡쳐
 // : 같은 이벤트 타입의 두 이벤트 핸들러가 한 요소에서 작동될 때 어떠한 일이 일어나는지를 기술하는 방법
@@ -73,3 +119,20 @@ function bgChange() {
 // 'DOMContentLoaded'
 // : 웹 문서(HTML)가 전체 로드될 때 실행
 // : HTML 코드가 끝까지 나열될 때
+document.addEventListener('DOMContentLoaded', function() {
+  let parentDiv = document.getElementById('parentDiv');
+  let childButton = document.getElementById('childButton');
+
+  // 캡쳐링의 경우 일반적인 상황에서 실행 X
+  // : 이벤트 리스너에 세 번째 인자로 true값을 전달할 때만 실행
+  parentDiv.addEventListener('click', function() {
+    console.log('부모 div의 이벤트가 실행됩니다.');
+  }, true);
+
+  childButton.addEventListener('click', function(e) {
+    console.log('자식 button의 이벤트가 실행됩니다.');
+    // 자식에서 부모로 진행(버블링)
+    e.stopPropagation();
+  }, true);
+});
+
