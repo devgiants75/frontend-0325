1ce39421fb3e70543c27ea6acdd6bc68e6a880ae
devgiants75 <devgiants75@naver.com>
2024-05-14 18:11:05 +0900

    20240514

---
+++ c_JAVASCRIPT/3주차/0514/b_practice/async_await.js
@@ -0,0 +1,7 @@
+// JSONPlaceholder
+// : 개발자들이 프론트엔드 코드를 테스트하고 프로토타이핑하는 데 사용할 수 있는 무료 REST API
+// >> 가짜 데이터를 제공하여 실제 서버에 데이터를 요청하는 것처럼 연습하거나 코드를 테스트 함에 도움
+// >> 게시글(posts), 댓글(comments), 사용자(users), 사진(photos) 등의 리소스를 JSON 형식으로 제공
+
+// Async/Await를 사용하여 JSONPlaceholder에서 데이터를 가져오는 함수
+
