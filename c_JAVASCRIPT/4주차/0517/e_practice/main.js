c78104d9c0e496bd945e2cd14fecff0a0bd99b30
devgiants75 <devgiants75@naver.com>
2024-05-21 21:51:13 +0900

    20240521

--- c_JAVASCRIPT/4주차/0517/e_practice/main.js
+++ c_JAVASCRIPT/4주차/0517/e_practice/main.js
@@ -1,1 +1,40 @@
 //! 쿠키 활용 문제
+document.addEventListener('DOMContentLoaded', () => {
+  const visitCountElement = document.getElementById('visitCount');
+
+  //& 방문 횟수를 업데이트하는 함수 정의
+  function updateVisitCount() {
+    let visits = getCookie('visits');
+    visits = visits ? parseInt(visits) + 1 : 1;
+
+    setCookie('visits', visits, 365);
+    visitCountElement.textContent = visits;
+  }
+
+  //& 쿠키 설정 함수 정의
+  function setCookie(name, value, days) {
+    let expires = '';
+    if (days) {
+      const date = new Date();
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      expires = 'expires=' + date.toUTCString();
+    }
+
+    document.cookie = name + '=' + value + ';' + expires + ';path=/';
+  }
+
+  //& 쿠키 가져오는 함수 정의
+  function getCookie(name) {
+    let nameEQ = name + '=';
+
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i].trim();
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+
+    return null;
+  }
+
+  updateVisitCount();
+});
