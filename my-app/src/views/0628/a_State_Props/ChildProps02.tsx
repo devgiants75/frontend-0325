a11e585b6885d77dc0a8f7367bab5b38962ce06f
devgiants75 <devgiants75@naver.com>
2024-06-28 19:49:07 +0900

    20240628

--- my-app/src/views/0628/a_State_Props/ChildProps02.tsx
+++ my-app/src/views/0628/a_State_Props/ChildProps02.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { UserType } from './StateProps02';
 
 // Props에 대한 타입 정의
+interface ChildComponentProps {
+  userInfo: UserType;
+  onUpdate: (newInfo: UserType) => void;
+}
 
-export default function ChildProps02() {
+export default function ChildProps02({ userInfo, onUpdate }: ChildComponentProps) {
+
+  let newData: UserType = {
+    username: '이승아',
+    height: 169
+  }
+
+  const updateInfo = () => {
+    // 부모로부터 전달받은 상태 업데이트를 사용
+    onUpdate(newData);
+  }
 
   return (
     <>
+      <p>사용자 이름: {userInfo.username}</p>
+      <p>사용자 이름: {userInfo.height}</p>
+      <button onClick={updateInfo}>사용자 정보 업데이트</button>
     </>
   )
 }
