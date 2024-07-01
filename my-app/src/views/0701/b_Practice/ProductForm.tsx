3c486e1c333c2b2bab2a45ce12583873c3e4e1a3
devgiants75 <devgiants75@naver.com>
2024-07-01 21:46:21 +0900

    20240701

--- my-app/src/views/0701/b_Practice/ProductForm.tsx
+++ my-app/src/views/0701/b_Practice/ProductForm.tsx
@@ -13,13 +13,32 @@ interface ProductFormProps {
 }
 
 function ProductForm({ addProduct }: ProductFormProps) {
-  const [name, setName] = useState('');
-  const [price, setPrice] = useState(0);
+  // const [name, setName] = useState('');
+  // const [price, setPrice] = useState(0);
+
+  //#
+  const [formState, setFormState] = useState<Omit<Product, 'id'>>({ name: '', price: 0 }); 
+
+  //#
+  const { name, price } = formState;
+
+  //#
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => { // 입력 필드의 상태를 업데이트하는 함수 정의
+    const { name, value } = e.target; // 이벤트 타겟에서 name과 value를 추출
+    setFormState(prevState => ({
+      ...prevState, // 이전 상태를 복사
+      [name]: name === 'price' ? parseFloat(value) : value // 필드의 이름에 따라 상태 업데이트
+    }));
+  };
 
   const handleAddProduct = () => {
-    addProduct({ name, price });
-    setName('');
-    setPrice(0);
+    // addProduct({ name, price });
+    // setName('');
+    // setPrice(0);
+
+    //#
+    addProduct({ name: name, price: price }); // addProduct 함수를 호출하여 상품 객체를 전달
+    setFormState({ name: '', price: 0 }); // 입력 필드를 초기화
   }
 
   return (
@@ -28,14 +47,18 @@ function ProductForm({ addProduct }: ProductFormProps) {
         type="text" 
         placeholder='Product Name'
         value={name}
-        onChange={e => setName(e.target.value)}
+        // onChange={e => setName(e.target.value)}
+        //#
+        onChange={handleInputChange}
         className={styles.input}
       />
       <input 
         type="number" 
         placeholder='Product Name'
         value={price}
-        onChange={e => setPrice(parseFloat(e.target.value))}
+        // onChange={e => setPrice(parseFloat(e.target.value))}
+        //#
+        onChange={handleInputChange}
         className={styles.input}
       />
       <button onClick={handleAddProduct} className={styles.button}>
