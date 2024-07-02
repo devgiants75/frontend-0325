9cc5076fe3d3018035bd5b0c961a5114c51f4007
devgiants75 <devgiants75@naver.com>
2024-07-02 21:46:24 +0900

    20240702

---
+++ my-app/src/views/0702/과제.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+//! 데이터 구조
+// - 전체 데이터: 배열
+// - 각 데이터: 객체 
+//   >> id: number
+//   >> input 데이터 string | number 2개 이상
+//   >> boolean값 1개 이상
+
+//! 기능
+// - create 생성, read 조회, update 수정, delete 삭제
+// - useState를 사용한 전체 배열 관리
+// - useRef를 사용한 id값 관리
+
+//! 컴포넌트
+// - main 컴포넌트 1개
+// - main 컴포넌트의 데이터와 기능을 props로 전달 받을 컴포넌트 2개 이상
+
+//! (+)
+// - input창은 하나의 핸들러로 관리
+// - map, filter 사용
+
+export default function Self() {
+  return (
+    <div>과제</div>
+  )
+}
