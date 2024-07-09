42a01de5e04def2abca6b6e9e026a75b0c7e775d
devgiants75 <devgiants75@naver.com>
2024-07-09 21:48:36 +0900

    20240709

--- my-app/src/components/slider/Slider.tsx
+++ my-app/src/components/slider/Slider.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+import './Slider.css';
 
 interface SliderProps {
   images: string[];
@@ -45,6 +46,18 @@ export default function Slider({ images }: SliderProps) {
   }, [currentIndex, isTransitioning]);
 
   return (
-    <div>Slider</div>
+    <div className="slider"> {/* 슬라이더 컨테이너 */}
+      <div className="slider-images" ref={sliderRef} onTransitionEnd={handleTransitionEnd}> {/* 슬라이더 이미지 컨테이너 */}
+        {images.map((image, index) => ( // 이미지 배열을 매핑하여 슬라이드를 생성
+          <div key={index} className="slide"> {/* 각 슬라이드 */}
+            <img src={image} alt={`Slide ${index}`} /> {/* 슬라이드 이미지 */}
+          </div>
+        ))}
+        {/* 첫 번째 이미지를 마지막에 복제하여 무한 루프 효과를 만듭니다. */}
+        <div className="slide">
+          <img src={images[0]} alt="Slide 0" /> {/* 첫 번째 슬라이드를 복제하여 추가 */}
+        </div>
+      </div>
+    </div>
   )
 }
