7ac1afea97f0b8843c06f6d0fe8999f1fd460394
devgiants75 <devgiants75@naver.com>
2024-07-10 20:18:13 +0900

    20240710

--- my-app/src/views/0710/b_Practice/Practice01.tsx
+++ my-app/src/views/0710/b_Practice/Practice01.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
 export default function Practice01() {
+
+  const [post, setPost] = useState<Post | null>(null);
+
+  const fetchPost = async () => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts/1"
+    );
+    // const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${userId}`);
+    const data: Post = await response.json();
+    setPost(data);
+  };
+
   return (
-    <div>Practice01</div>
+    <div>
+      <button onClick={fetchPost}>Load Post</button>
+      <div>
+        {post ? (
+          <div>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </div>
+        ) : (
+          <p>No data loaded</p>
+        )}
+      </div>
+    </div>
   )
 }
