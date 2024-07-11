58a93cdefdc1585e7e917f10dddfbb53e6690b8b
devgiants75 <devgiants75@naver.com>
2024-07-11 19:41:08 +0900

    20240711

---
+++ my-app/src/views/projects/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Board from './Board/Board';
+import Todo from './Todo/index';
+
+export default function index() {
+  return (
+    <div>
+      <Board />
+      <Todo />
+    </div>
+  )
+}
