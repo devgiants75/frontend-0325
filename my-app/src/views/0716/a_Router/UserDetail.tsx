690afc112a494113b9a8f67821f99cf69b708075
devgiants75 <devgiants75@naver.com>
2024-07-16 19:38:32 +0900

    20240716

--- my-app/src/views/0716/a_Router/UserDetail.tsx
+++ my-app/src/views/0716/a_Router/UserDetail.tsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-interface User {
+type User = {
   id: number;
   name: string;
   email: string;
   phone: string;
 }
 
-export default function UserDetail() {
+function UserDetail() {
+  const { id } = useParams<{ id: string }>();
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then(response => response.json())
+      .then(data => setUser(data));
+  }, [id]);
+
   return (
-    <div>UserDetail</div>
-  )
+    <div>
+      {user ? (
+        <div>
+          <h1>{user.name}</h1>
+          <p>Email: {user.email}</p>
+          <p>Phone: {user.phone}</p>
+        </div>
+      ) : (
+        <p>Loading user details...</p>
+      )}
+    </div>
+  );
 }
+
+export default UserDetail;
--- my-app/src/views/0716/a_Router/UserList.tsx
+++ my-app/src/views/0716/a_Router/UserList.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-interface User {
+type User = {
   id: number;
   name: string;
 }
 
-export default function UserList() {
+function UserList() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setUsers(data));
+  }, []);
+
   return (
-    <div>UserList</div>
-  )
+    <ul>
+      {users.map(user => (
+        <li key={user.id}>
+          <Link to={`${user.id}`}>{user.name}</Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
+
+export default UserList;
