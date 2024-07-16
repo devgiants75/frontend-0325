37a47f230fe9c3dd65f0048dac55e3ac78cece3f
devgiants75 <devgiants75@naver.com>
2024-07-16 18:18:50 +0900

    20240716

---
+++ my-app/src/views/0716/c_Axios/Axios02.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Axios02() {
+  // HTTP 요청으로 가져오는 사용자 데이터
+  
+  // HTTP 요청 중 발생하는 error에 대한 상태 관리
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ my-app/src/views/0716/c_Axios/Axios03.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+//! Axios 요청 - POST
+export default function Axios03() {
+
+  return (
+    <></>
+  )
+}
